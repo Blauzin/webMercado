dc3412fb3fd39dacb75c0181f00cdbf78b854e25
Blauzin <62842925+Blauzin@users.noreply.github.com>
2023-11-06 10:48:22 -0300

    nc

--- carrinho-final/php/novoProduto.php
+++ carrinho-final/php/novoProduto.php
@@ -7,27 +7,26 @@
   $arquivo = $_FILES["arquivo"];
 
   if($arquivo["type"] == "image/png")
-      {
+  {
         $novo_arquivo = "../imagens/".$arquivo["name"];
-        move_uploaded_file($arquivo["tmp_name"], "../imagens/".$arquivo["name"]);
+        if(move_uploaded_file($arquivo["tmp_zname"], $novo_arquivo))
+        {
+              $conexao = criarConexao();
+              $query = "INSERT INTO produtos(nome, preco) VALUES('$nome', '$preco')";
+              mysqli_query($conexao, $query);
 
-        $conexao = criarConexao();
-        $query = "INSERT INTO produtos(nome, preco) VALUES('$nome', '$preco')";
-        mysqli_query($conexao, $query);
+              $mensagem = "Upload feito com sucesso";
+            }
+            else
+            {
+              $mensagem = "Erro ao fazer upload do arquivo";
+            }
+          }
+          else
+          {
+            $mensagem = "Somente é permitido pngs!";  
+          }
 
-        $mensagem = "Upload feito com suceso";
-      }
-  else
-      {
-        $mensagem = "Somente e permitido pngs!";  
-  
-      }
-
-
-  $json = json_encode($mensagem);
-  echo $json;
-
-
-
-
-?>
+          $json = json_encode($mensagem);
+          echo $json;
+        ?>
